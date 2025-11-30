623b6a82f12cee7f7167cb8f9b001e1865fa2835
sparklerfish <julie.ellen.adams@gmail.com>
2025-11-29 21:08:23 -0800

    refactor

--- 2-pt-frontlight-update.lua
+++ 2-pt-frontlight-update.lua
@@ -6,14 +6,26 @@
 local userpatch = require("userpatch")
 
 local function patchFrontlightUpdate()
-    local Menu = require("ui/widget/menu")
     local UIManager = require("ui/uimanager")
     local FileManager = require("apps/filemanager/filemanager")
 
-    local function updateFileManagerFooter()
-        for i = #UIManager._window_stack, 1, -1 do
-            local widget = UIManager._window_stack[i].widget
-            if widget.file_chooser and widget.file_chooser.updatePageInfo then
+    local function updateFileManagerFooter(target_widget)
+        -- If called with a specific widget that has file_chooser, use it directly
+        if target_widget and target_widget.file_chooser and target_widget.file_chooser.updatePageInfo then
+            target_widget.file_chooser:updatePageInfo()
+            UIManager:setDirty(target_widget, function()
+                return "ui", target_widget.dimen
+            end)
+            return
+        end
+
+        -- Fallback: search window stack (uses internal API, may break in future versions)
+        local window_stack = UIManager._window_stack
+        if not window_stack then return end
+
+        for i = #window_stack, 1, -1 do
+            local widget = window_stack[i].widget
+            if widget and widget.file_chooser and widget.file_chooser.updatePageInfo then
                 widget.file_chooser:updatePageInfo()
                 UIManager:setDirty(widget, function()
                     return "ui", widget.dimen
@@ -23,21 +35,10 @@ local function patchFrontlightUpdate()
         end
     end
 
-    if Menu and not Menu._frontlight_patch_applied then
-        Menu._frontlight_patch_applied = true
-        Menu.onFrontlightStateChanged = function(self)
-            if self.cur_folder_text then
-                updateFileManagerFooter()
-            end
-        end
-    end
-
     if FileManager and not FileManager._frontlight_patch_applied then
         FileManager._frontlight_patch_applied = true
         FileManager.onFrontlightStateChanged = function(self)
-            if self.file_chooser then
-                updateFileManagerFooter()
-            end
+            updateFileManagerFooter(self)
         end
     end
 end
